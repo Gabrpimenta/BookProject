f89a65e0b12d3128b9027eb14f38dcce6c649426
Gabrpimenta <gabrielrpimenta@gmail.com>
2023-02-21 14:02:21 -0300

    Create Category done

--- BookWeb/Models/Category.cs
+++ BookWeb/Models/Category.cs
@@ -1,16 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookWeb.Models
-{
-    public class Category
-    {
-        [Key]
-        public int Id { get; set; }
+	{
+	public class Category
+		{
+		[Key]
+		public int Id { get; set; }
 
-        [Required]
-        public string Name { get; set; }
+		[Required]
+		public string Name { get; set; }
 
-        public int DisplayOrder { get; set; }
-        public DateTime CreatedDateRime { get; set; } = DateTime.Now;
-    }
-}
+		[DisplayName("Display Order")]
+		[Range(1, 1000, ErrorMessage = "Display Order must be between 1 and 100.")]
+		public int DisplayOrder { get; set; }
+
+		public DateTime CreatedDateRime { get; set; } = DateTime.Now;
+		}
+	}
