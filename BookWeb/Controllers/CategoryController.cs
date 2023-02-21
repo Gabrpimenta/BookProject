33464624853df4b30019be2610189feb3bc18017
Gabrpimenta <gabrielrpimenta@gmail.com>
2023-02-21 14:42:50 -0300

    Update and Delete complete

--- BookWeb/Controllers/CategoryController.cs
+++ BookWeb/Controllers/CategoryController.cs
@@ -46,5 +46,71 @@ namespace BookWeb.Controllers
 				}
 			return View();
 			}
+
+		//UPDATE
+		public IActionResult Edit(int? id)
+			{
+			if (id == null || id == 0)
+				{
+				return NotFound();
+				}
+			var categoryFromDb = _db.Categories.Find(id);
+			if (categoryFromDb == null)
+				{
+				return NotFound();
+				}
+			return View(categoryFromDb);
+			}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(Category obj)
+			{
+			if (obj.Name == obj.DisplayOrder.ToString())
+				{
+				ModelState.AddModelError(
+						"CustomError",
+						"The DisplayOrder cannot exactly match the Name."
+				);
+				}
+
+			if (ModelState.IsValid)
+				{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+				}
+			return View(obj);
+			}
+
+		//DELETE
+		public IActionResult Delete(int? id)
+			{
+			if (id == null || id == 0)
+				{
+				return NotFound();
+				}
+			var categoryFromDb = _db.Categories.Find(id);
+			if (categoryFromDb == null)
+				{
+				return NotFound();
+				}
+			return View(categoryFromDb);
+			}
+
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeletePOST(int? id)
+			{
+			var obj = _db.Categories.Find(id);
+			if (obj == null)
+				{
+				return NotFound();
+				}
+
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+			}
 		}
 	}
