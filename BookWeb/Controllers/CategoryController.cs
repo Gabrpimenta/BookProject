cd6a1e021131c69d6463e08885e2e4ebfa02a046
Gabrpimenta <gabrielrpimenta@gmail.com>
2023-02-21 15:58:14 -0300

    Final changes

--- BookWeb/Controllers/CategoryController.cs
+++ BookWeb/Controllers/CategoryController.cs
@@ -42,6 +42,7 @@ namespace BookWeb.Controllers
 				{
 				_db.Categories.Add(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Category created successfully.";
 				return RedirectToAction("Index");
 				}
 			return View();
@@ -78,6 +79,7 @@ namespace BookWeb.Controllers
 				{
 				_db.Categories.Update(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Category updated successfully.";
 				return RedirectToAction("Index");
 				}
 			return View(obj);
@@ -110,6 +112,7 @@ namespace BookWeb.Controllers
 
 			_db.Categories.Remove(obj);
 			_db.SaveChanges();
+			TempData["success"] = "Category deleted successfully.";
 			return RedirectToAction("Index");
 			}
 		}
